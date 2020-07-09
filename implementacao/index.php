7d9c603e87a00dddb366d381248768bc35ea084e
Raimundo Martins <68038164+rmdevsolutions@users.noreply.github.com>
2020-07-08 23:44:25 -0300

    Desenvolvimento FrontEnd
    Inicio de desenvolvimento frontend utilizando framework bootstrap

---
+++ implementacao/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/init.css">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600;700;900&display=swap" rel="stylesheet">
+  <title></title>
+</head>
+<body>
+
+  <nav>
+    <div class="header elevacao">
+      <div class="row h-100">
+        <div class="col-md-4">
+          <div class="logo">
+
+          </div>
+        </div>
+        <div class="col-md-8">
+          <span class="letreiro-header">CAIXA LIVRE</span>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <section>
+    <div class="comandos">
+      <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-2">
+          <div class="button-seletor">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
